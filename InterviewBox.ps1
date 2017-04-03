401fd2845c555a391e16c1eb541bfb48763ef8be
Josh <josh.quintus@gmail.com>
2017-04-03 11:01:26 -0400

    Update InterviewBox.ps1

--- InterviewBox.ps1
+++ InterviewBox.ps1
@@ -5,9 +5,9 @@ $Boxstarter.NoPassword=$false # Is this a machine with no login password?
 $Boxstarter.AutoLogin=$true   # Save my password securely and auto-login after a reboot
 
 # Set the computer name
-$computerName = "InterviewPC"
-Write-Output "Setting computer name to $computerName"
-Rename-Computer $computerName
+# $computerName = "InterviewPC"
+# Write-Output "Setting computer name to $computerName"
+# Rename-Computer $computerName
 
 choco install -y eclipse
 choco install -y chrome
