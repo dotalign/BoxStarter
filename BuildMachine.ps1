c18b1560882a91b342bedd3d27aa0230f9cfab54
Josh Quintus <josh@dotalign.com>
2017-05-26 14:51:29 -0400

    Add definition for a build machine.
    It's pretty light for now.

---
+++ BuildMachine.ps1
@@ -0,0 +1,9 @@
+# To Run From Web:  http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/dotalign/BoxStarter/master/DevMachine.ps1
+
+# Before running this script install:
+# Visual Studio 2017 
+# Box https://www.box.com/resources/downloads
+
+cinst -y KickAssVim
+cinst -y notepadplusplus.install
+cinst -y wixtoolset
